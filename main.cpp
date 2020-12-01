7705341686ded437aeb76cc2e0feab9f36cc4f60
Vince Duarte <vinceduarte3@gmail.com>
2020-12-01 14:19:42 -0500

    added loop to run mutiple options settings.json added iostream

--- main.cpp
+++ main.cpp
@@ -3,25 +3,32 @@
 using namespace std;
 
 int main() {
-    int option;
-    // menu
-    cout << "1. Search for a Senator's name" << endl;
-    cout << "2. Exit" << endl;
-    // option selection
-    cout << "Enter one of the options above: ";
-    cin >> option;
-    
+    bool quit = true;
+
     // loop until exit menu
-    while (option =! 2) {
+    while (quit) {
         
+        int option;
+        // menu
+        cout << "U.S. Senator Stock Market Data" << endl;
+        cout << "1. Search for a Senator's records" << endl;
+        cout << "2. Exit" << endl;
+        
+        // option selection
+        cout << "Enter one of the options above: ";
+        cin >> option;
+
         // function calls
         if (option == 1) {
+            
             // Search the csv for Senator records
 
         }
-        /*else if () {
-
-        }*/
+        else if (option == 2) {
+            
+            // Exit the loop
+            quit = false;
+        }
         else {
             cout << "Not a valid option, try again."
         }
