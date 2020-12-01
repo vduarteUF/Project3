c5212aa0950238a5abda23478e1efea61ebd7101
Vince Duarte <vinceduarte3@gmail.com>
2020-12-01 14:13:30 -0500

    first menu declaration

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 using namespace std;
 
 int main() {
+    int option;
+    // menu
+    cout << "1. Search for a Senator's name" << endl;
+    cout << "2. Exit" << endl;
+    // option selection
+    cout << "Enter one of the options above: ";
+    cin >> option;
     
+    // loop until exit menu
+    while (option =! 2) {
+        
+        // function calls
+        if (option == 1) {
+            // Search the csv for Senator records
+
+        }
+        /*else if () {
+
+        }*/
+        else {
+            cout << "Not a valid option, try again."
+        }
+    }
     return 0;
 }
